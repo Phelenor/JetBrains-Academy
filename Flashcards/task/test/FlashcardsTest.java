8ae97c1e0c1b9c0b62d7fc5356a9068fbfad4df4
Phelenor <62450771+Phelenor@users.noreply.github.com>
2021-01-30 14:35:50 +0100

    Removing bloat

--- Flashcards/task/test/FlashcardsTest.java
+++
@@ -1,8 +0,0 @@
-import flashcards.MainKt;
-import org.hyperskill.hstest.v6.stage.BaseStageTest;
-
-public abstract class FlashcardsTest<T> extends BaseStageTest<T> {
-    public FlashcardsTest() {
-        super(MainKt.class);
-    }
-}
--- Parking Lot/task/test/ParkingTest.java
+++
@@ -1,8 +0,0 @@
-import org.hyperskill.hstest.v6.stage.BaseStageTest;
-import parking.MainKt;
-
-public abstract class ParkingTest<T> extends BaseStageTest<T> {
-    public ParkingTest() {
-        super(MainKt.class);
-    }
-}
